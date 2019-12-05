b0e6557a7c0383134e9afd3d5307c0c5578962a0
Atanas Andonov <andonov.atanas98@gmail.com>
2019-12-05 21:50:22 +0200

    Fixed paths

--- gulpfile.js
+++ gulpfile.js
@@ -55,8 +55,9 @@ gulp.task("watch", function() {
 	gulp.watch( 'docs/**/*.js' ).on('change', browserSync.reload );
 });
 
-gulp.task("default", gulp.series('jekyll-dev', 'sass', 'watch'));
 
 gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
 	return cp.spawn('git status && git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
 }));
+
+gulp.task("default", gulp.series('jekyll-dev', 'sass', 'watch'));
